b1ce3726d68653b1d1ba9baf22e69b418283b7df
Ivan Nj <ivannhrro@gmail.com>
2025-02-10 02:13:22 +0100

    project update

---
+++ README.md
@@ -0,0 +1,21 @@
+
+
+## React JS, NextJS, Tailwind CSS, Framer Motion, Leaflet JS, Swiper JS.
+
+```bash
+# Step 1 - Install dependences
+yarn
+# Step 2 - See the result in your browser on localhost:3000
+yarn dev
+```
+
+![Screenshot 1](IMG_README/homepage.png)
+
+![Screenshot 2](IMG_README/housepage.png)
+
+
+## Deploy 
+
+```bash
+yarn build
+```
---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        secondary: "#727C73",
+      },
+      backgroundImage: {
+        banner: "url('/assets/house-banner.png')",
+      },
+      boxShadow: {
+        light: "0px 4px 30px rgba(0, 0, 0, 0.08)",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
